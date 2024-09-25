bd8ef5d51ad82155351476a2c943a66b28b6f277
aixion1506 <aixion1506@gmail.com>
2024-09-25 14:59:34 +0900

    인생 처음 Java를 Java라

---
+++ src/_01_basic_syntax/data_types.java
@@ -0,0 +1,10 @@
+package _01_basic_syntax;
+
+//  Primitive Type (기본형)
+// - 사용되기 전에 선언되어야 함
+// - OS에 따라 자료형 길이가 변하지 않는다
+// - "비객체" 타입 => Null 값 소유 불가(Null도 객체 취급)
+
+//  Reference Type (참조형)
+// - java.lang.Object 클래스를 상속받음
+// - 기본형이 아니면 모두 참조형
