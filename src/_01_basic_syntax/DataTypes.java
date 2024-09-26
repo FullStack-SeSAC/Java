467a0fadb069c6ac91e81a1b597b9bbf5932a95b
aixion1506 <aixion1506@gmail.com>
2024-09-26 19:11:21 +0900

    Study: Java DataTypes class

--- src/_01_basic_syntax/DataTypes.java
+++ src/_01_basic_syntax/DataTypes.java
@@ -27,8 +27,7 @@ public class DataTypes {
         // 문자형 변수 선언
         char ch = 'A'; // 작은 따옴표 사용
 
-        // 논리형 변수
-        boolean bool = true;
+//        boolean bool = true;
 
         // 변수 출력
         System.out.println("정수형 변수: " + x + ", " + y + ", " + z + ", " + b);
@@ -36,5 +35,47 @@ public class DataTypes {
         System.out.println("실수형 변수: " + a + ", " + c);
         System.out.println("문자형 변수: " + ch);
         System.out.println("논리형 변수: " + ch);
+
+        // Reference Type (참조형)
+        System.out.println("==== Reference Type ====");
+        
+        // Java에서는 String 타입은 특별 취급 => 참조형인데 기본형처럼 사용
+        String greeting = "Hello, World!";
+
+        // 배열 (Array)
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        // 클래스
+        Person person = new Person("rani", 13);
+
+        System.out.println("String 변수: " + greeting);
+        System.out.println("배열 변수 : " +  numbers );
+
+        for(int i = 0 ; i <numbers.length; i ++){
+            System.out.println(numbers[i]);
+        }
+        System.out.println("Person 객체 " +person.getName() + ", " + person.getAge() );
     }
 }
+
+
+
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {return  name;}
+
+    public int getAge() {return age;}
+}
+
+
+
+
+
+
